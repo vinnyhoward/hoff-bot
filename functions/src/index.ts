f625e1e45549e80866fd2c331e9628c516d2ebc5
Vince Howard <vincenguyenhoward@gmail.com>
2020-06-28 13:23:34 -0600

    added logs for debugging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,19 +5,17 @@ import { WebClient } from '@slack/web-api';
 const bot = new WebClient(functions.config().slack.token);
 
 const { PubSub } = require('@google-cloud/pubsub');
-const puSubClient = new PubSub();
+const pubsub = new PubSub();
 
 export const hoffBot = functions.https.onRequest(async (req, res) => {
+    console.log('hoffBot - req:', req);
     // Validate Signature
     verifySignature(req); // See snippet above for implementation
 
     const data = JSON.stringify(req.body);
     const dataBuffer = Buffer.from(data);
-
-    await puSubClient
-        .topic('slack-channel-join')
-        .publisher()
-        .publish(dataBuffer);
+    console.log('dataBuffer:', dataBuffer);
+    await pubsub.topic('slack-channel-join').publisher().publish(dataBuffer);
 
     res.sendStatus(200);
 });
@@ -29,14 +27,17 @@ export const slackChanelJoin = functions.pubsub
         const { user, channel } = event;
         const generalChannel = '#general';
         const newChannel = '#random';
-
+        console.log(
+            'generalChannel === newChannel:',
+            channel !== generalChannel,
+        );
         if (channel !== generalChannel) {
             throw Error();
         }
 
         const userResult: any = await bot.users.profile.get({ user });
         const { display_name } = userResult.profile;
-
+        console.log('userResult:', userResult);
         await bot.channels.invite({
             channel: newChannel,
             user,
